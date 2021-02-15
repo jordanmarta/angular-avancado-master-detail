4497149c1fc5d93c859b7e60f1c2897100d31403
Jordan Marta <jordan.smta@gmail.com>
2021-02-15 12:45:31 -0300

    Finalizando primeira parte

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -5,6 +5,8 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { map, catchError, flatMap } from 'rxjs/operators';
 
+import { CategoryService } from '../../categories/shared/category.service';
+
 import { Entry } from './entry.model';
 
 @Injectable({
@@ -13,10 +15,12 @@ import { Entry } from './entry.model';
 export class EntryService {
   private apiPath: string = 'api/entries';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private categoryService: CategoryService) {}
 
   getAll(): Observable<Entry[]> {
-    return this.http.get(this.apiPath).pipe(catchError(this.handleError), map(this.jsonDataToEntries));
+    return this.http
+      .get(this.apiPath)
+      .pipe(catchError(this.handleError), map(this.jsonDataToEntries));
   }
 
   getById(id: number): Observable<Entry> {
@@ -26,13 +30,29 @@ export class EntryService {
   }
 
   create(entry: Entry): Observable<Entry> {
-    return this.http.post(this.apiPath, entry).pipe(catchError(this.handleError), map(this.jsonDataToEntry));
+    return this.categoryService.getById(entry.categoryId).pipe(
+      flatMap((category) => {
+        entry.category = category;
+
+        return this.http
+          .post(this.apiPath, entry)
+          .pipe(catchError(this.handleError), map(this.jsonDataToEntry));
+      })
+    );
   }
 
   update(entry: Entry): Observable<Entry> {
     const url = `${this.apiPath}/${entry.id}`;
 
-    return this.http.put('lawdeasd', entry).pipe(catchError(this.handleError), map(this.jsonDataToEntry));
+    return this.categoryService.getById(entry.categoryId).pipe(
+      flatMap((category) => {
+        entry.category = category;
+
+        return this.http
+          .put(url, entry)
+          .pipe(catchError(this.handleError), map(this.jsonDataToEntry));
+      })
+    );
   }
 
   delete(id: number): Observable<any> {
@@ -50,7 +70,7 @@ export class EntryService {
     const entries: Entry[] = [];
     jsonData.forEach((element) => {
       const entry = Object.assign(new Entry(), element);
-      entries.push(entry)
+      entries.push(entry);
     });
     return entries;
   }
