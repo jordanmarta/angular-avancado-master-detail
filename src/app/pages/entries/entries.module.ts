60d69578eff862dacce7bd110c943a11b2b2ec18
Jordan Marta <jordan.smta@gmail.com>
2021-02-16 08:01:50 -0300

    improving BaseResourceService && refactoring CategoryService

--- src/app/pages/entries/entries.module.ts
+++ src/app/pages/entries/entries.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../../shared/shared.module';
 
 import { EntriesRoutingModule } from './entries-routing.module';
-
 import { EntryListComponent } from './entry-list/entry-list.component';
 import { EntryFormComponent } from './entry-form/entry-form.component';
 
